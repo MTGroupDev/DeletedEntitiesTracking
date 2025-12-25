cf42405acd96c104e4914551c6690e52c76a7f3c
Serenite1337 <vampirpir53@gmail.com>
2025-12-25 09:36:26 +0300

    Собрано и опубликовано на версии 25.3
    Собрано и опубликовано на версии 25.3

---
+++ mtg.Administration/mtg.Administration.Server/Reports/DeletionsDocumentReport/Queries/CreateTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE {0}
+(ReportSessionId varchar NOT NULL,
+DocumentId bigint NULL,
+DocumentName varchar NULL,
+Employee varchar NULL,
+HostName varchar NULL,
+Date TIMESTAMP NULL,
+EntityType varchar NULL,
+SourceType varchar NULL)
